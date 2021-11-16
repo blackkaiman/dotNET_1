fc6cc30680389363e86fdc4b8739c711a626cdd6
Kaiman <demetriustalos@gmail.com>
2021-11-16 16:49:01 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace dotNET_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            int? a = 5;  //value type
+            string b = "5";//reference type
+            int c = Convert.ToInt32(b);
+            Console.WriteLine("Exemplu conversie explicita - unboxing" + c);
+            int x = 6;
+            long d = x;
+            Console.WriteLine("Exemplu conversie implicita - boxing " + d);
+            b = null;
+            a = null;
+            Console.WriteLine("Exemplu nullable string vs int " + b + " " + a);
+        }
+    }
+}
